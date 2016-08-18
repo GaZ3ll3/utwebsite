45ce5063477b03573838fd8120a4d45a62982788
Yimin <zhongym8903@gmail.com>
2016-08-17 22:52:32 -0500

    update

--- javascripts/main.js
+++
@@ -1,8 +0,0 @@
-var config = [
-  { left: '$$', right: '$$', display: true  },
-  { left: '$',  right: '$',  display: false }
-]
-var articles = document.querySelectorAll('article.post-content');
-for (var i = 0; i < articles.length; i++) {
-  renderMathInElement(articles.item(i), { delimiters: config });
-}
